ca179719e6aa6ed36c1711ca9e638616800d5211
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-02 14:06:58 +0100

    adjusted styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,12 @@
 const Navbar = () => {
   return (
-    <nav className="py-6 shadow-md">
-      <div className="container mx-auto">
-        <div className="flex justify-between px-4 md:px-32">
-          <h2 className="text-md font-bold md:text-2xl">Where in the world?</h2>
-          <span className="flex gap-x-2 items-center">
-            <i className="fa fa-moon">&#xf186;</i>
-            <p className="font-semibold text-sm">Dark Mode</p>
-          </span>
-        </div>
+    <nav className="py-6 shadow-md w-full">
+      <div className="container mx-auto flex justify-between px-2 md:px-10">
+        <h2 className="text-md font-bold md:text-2xl">Where in the world?</h2>
+        <span className="flex gap-x-2 items-center">
+          <i className="fa fa-moon">&#xf186;</i>
+          <p className="font-semibold text-sm">Dark Mode</p>
+        </span>
       </div>
     </nav>
   );
--- src/screens/Hompage.js
+++ src/screens/Hompage.js
@@ -15,9 +15,9 @@ const HomePage = () => {
   }, []);
 
   return (
-    <main className=" py-6">
+    <main className="py-6">
       <div className="mx-auto flex flex-col container">
-        <div className="px-8 md:px-32 flex flex-col items-center flex-wrap lg:flex-row lg:justify-between">
+        <div className="px-10 flex items-center flex-wrap justify-between">
           <CardList countries={countries} />
         </div>
       </div>
