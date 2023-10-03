4e9e7fda83c24360571b042f696b7aa7f4bc84e0
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-03 19:23:22 +0100

    created reusable Container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,5 +1,8 @@
-const Container = () => {
-  return <div></div>;
+import classNames from "classnames";
+
+const Container = ({ className, children }) => {
+  const classes = classNames(className, "container mx-auto");
+  return <div className={classes}>{children}</div>;
 };
 
 export default Container;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
+import Container from "./Container";
+
 const Navbar = () => {
   return (
     <nav className="py-6 shadow-md w-full">
-      <div className="container mx-auto flex justify-between px-2 md:px-10">
+      <Container className="flex justify-between px-2 md:px-10">
         <h2 className="text-md font-bold md:text-2xl">Where in the world?</h2>
         <span className="flex gap-x-2 items-center cursor-pointer">
           <i className="fa fa-moon">&#xf186;</i>
           <p className="font-semibold text-sm">Dark Mode</p>
         </span>
-      </div>
+      </Container>
     </nav>
   );
 };
