f46c2e8eda33618fe88b6b5eeeff5d0c1e6e644e
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-03 22:25:17 +0100

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Navbar from "./components/Navbar";
 import HomePage from "./screens/Hompage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Navbar></Navbar>
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
