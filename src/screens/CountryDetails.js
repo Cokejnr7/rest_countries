4c15386edeaf470bdb9d6b678604aba8151fb644
Kolawole Daniel <kolawolecoke@gmail.com>
2023-10-06 11:30:20 +0100

    fixed responsive layout

--- src/screens/CountryDetails.js
+++ src/screens/CountryDetails.js
@@ -5,10 +5,10 @@ const CouuntryDetails = () => {
     <Container className={"flex flex-col"}>
       Country Details
       <div className="flex flex-wrap justify-between ">
-        <img src="https://flagcdn.com/as.svg" alt="" className="lg:w-1/2" />
-        <div className="flex flex-col justify-center">
+        <img src="https://flagcdn.com/as.svg" alt="" className="xl:w-1/2" />
+        <div className="flex flex-col justify-center py-6">
           <h2 className="mb-8 font-bold text-3xl">Belgium</h2>
-          <div className="flex space-x-20 mb-10">
+          <div className="flex gap-x-20 gap-y-8 mb-10 flex-wrap">
             <ul>
               <li>
                 <span className="font-semibold text-lg">Native Name: </span>
@@ -30,6 +30,7 @@ const CouuntryDetails = () => {
                 Brussels
               </li>
             </ul>
+
             <ul>
               <li>
                 <span className="font-semibold text-lg">
@@ -48,12 +49,12 @@ const CouuntryDetails = () => {
             </ul>
           </div>
 
-          <div>
+          <div className="flex gap-x-3 items-center flex-wrap">
             <span className="font-semibold text-lg">Border Countries:</span>
-            <ul className="flex gap-x-3 items-center rounded">
-              <li className="py-1 px-4 shadow-sm rounded">France</li>
-              <li className="py-1 px-4 shadow-sm rounded">Germany</li>
-              <li className="py-1 px-4 shadow-sm rounded">Netherlands</li>
+            <ul className="flex gap-x-3 items-center rounded flex-wrap">
+              <li className="py-1 px-4 shadow-md rounded">France</li>
+              <li className="py-1 px-4 shadow-md rounded">Germany</li>
+              <li className="py-1 px-4 shadow-md rounded">Netherlands</li>
             </ul>
           </div>
         </div>
